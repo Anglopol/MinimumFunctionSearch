eb55c049bda3c146eac4bbf6d28797050190b0a6
ivan <vertvery@gmail.com>
2018-11-13 19:50:27 +0300

    complete golden search method

--- src/methods/GoldenSectionSearch.java
+++ src/methods/GoldenSectionSearch.java
@@ -15,14 +15,36 @@ public class GoldenSectionSearch extends AbstractMethod {
 
     @Override
     protected void setPoints() {
+        setFirstPoint();
+        setSecondPoint();
+    }
+
+    private void setFirstPoint() {
         firstPoint = startOfInterval + ((3 - Math.sqrt(5)) / 2)
                 * (endOfInterval - startOfInterval);
+    }
+
+    private void setSecondPoint() {
         secondPoint = startOfInterval + ((Math.sqrt(5) - 1) / 2)
                 * (endOfInterval - startOfInterval);
     }
 
     @Override
     public double getFunctionMinimum() {
-        return 0;
+        double ai = Function.functionAtPoint(firstPoint), bi = Function.functionAtPoint(secondPoint);
+        while (Math.abs(endOfInterval - startOfInterval) > Function.EPSILON) {
+            if (ai >= bi) {
+                startOfInterval = firstPoint;
+                firstPoint = secondPoint;
+                setSecondPoint();
+                ai = Function.functionAtPoint(firstPoint);
+            } else {
+                endOfInterval = secondPoint;
+                secondPoint = firstPoint;
+                setFirstPoint();
+                bi = Function.functionAtPoint(secondPoint);
+            }
+        }
+        return (startOfInterval + endOfInterval) / 2;
     }
 }
